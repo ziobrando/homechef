2bd53ce281137afdec1ddf91a1c32bbf59e1643f
Alberto Brandolini <alberto.brandolini@gmail.com>
2021-02-17 11:26:50 +0100

    Tolta dipendenza

--- src/test/java/it/avanscoperta/homechef/ShopperServiceTest.java
+++ src/test/java/it/avanscoperta/homechef/ShopperServiceTest.java
@@ -1,15 +1,12 @@
 package it.avanscoperta.homechef;
 
-import it.avanscoperta.homechef.menu.domain.ShopperService;
-import it.avanscoperta.homechef.users.User;
-import it.avanscoperta.homechef.users.builders.UserBuilder;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
